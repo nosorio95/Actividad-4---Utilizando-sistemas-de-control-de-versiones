d19c476a79b58fc6e46b0bb41a4467b9205216cb
nosorio95 <93720864+nosorio95@users.noreply.github.com>
2025-11-29 09:56:38 -0500

    feat(raiz): Implementada la raiz cuadrada (con import math).

--- calculadora-actividad4/utils.py
+++ calculadora-actividad4/utils.py
@@ -11,3 +11,8 @@ def dividir(a, b):
     if b == 0:
         return "Error: División por cero"
     return a / b
+
+def raiz_cuadrada(a):
+    if a < 0:
+        return "Error: Raíz de número negativo"
+    return math.sqrt(a)
